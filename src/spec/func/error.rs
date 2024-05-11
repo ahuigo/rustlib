c484df362e95608655b3a73ef57435275e833cbe
ahuigo <1781999+ahuigo@users.noreply.github.com>
2024-05-11 21:41:44 +0800

    feat: if err

--- src/spec/func/error.rs
+++ src/spec/func/error.rs
@@ -13,7 +13,6 @@ mod err_panic {
         v[99];
     }
 
-
     /* 2. 改成panic 直接终止: abort
     配置修改 Cargo.toml 文件，实现在 release 模式下遇到 panic 直接终止：
     #![allow(unused)]
@@ -47,4 +46,62 @@ fn err_result() {
     // let _home: IpAddr = "127.0.0.1".parse().unwrap();
     let rst = "127.0.0.1".parse(); // Result<F, F::Err>
     let _home: IpAddr = rst.unwrap(); //失败时直接unwrap_failed:panic
+}
+
+/**
+golang 避免多次if err != nil　方法1(缺点是要临时定义write和err)：
+    var err error
+    write := func(buf []byte) {
+        if err != nil {
+            return
+        }
+        _, err = w.Write(buf)
+    }
+    write(p0[a:b])
+    write(p1[c:d])
+    write(p2[e:f])
+    // and so on
+    if err != nil {
+        return err
+    }
+
+方法2: 缺点是只适合多次访问同一对象的方法：
+
+    func (ew *errWriter) write(buf []byte) {
+        if ew.err != nil {
+            return
+        }
+        _, ew.err = ew.w.Write(buf)
+    }
+    ew := &errWriter{w: fd}
+    ew.write(p0[a:b])
+    ew.write(p1[c:d])
+    ew.write(p2[e:f])
+    // and so on
+    if ew.err != nil {
+        return ew.err
+    }
+
+ **/
+#[test] // write_all()? 如果遇到错误，直接返回错误
+fn if_err() {
+    use std::io::Write;
+    #[allow(unused)]
+    fn write_all(
+        fd: &mut dyn Write,
+        p0: &[u8],
+        a: usize,
+        b: usize,
+        p1: &[u8],
+        c: usize,
+        d: usize,
+        p2: &[u8],
+        e: usize,
+        f: usize,
+    ) -> std::io::Result<()> {
+        fd.write_all(&p0[a..b])?;
+        fd.write_all(&p1[c..d])?;
+        fd.write_all(&p2[e..f])?;
+        Ok(())
+    }
 }
